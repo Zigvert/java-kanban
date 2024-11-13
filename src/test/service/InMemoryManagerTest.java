ef4e7e33118e0975d34014d16644149cfc231bfe
Zigvert <dark133788@gmail.com>
2024-11-13 14:41:31 +0300

    Fixed

--- src/test/service/InMemoryManagerTest.java
+++ src/test/service/InMemoryManagerTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class InMemoryManagerTest {
     @DisplayName("Создание таскМенеджера")
     @Test
-    void getDefaultTest(){
+    void getDefaultTest() {
         TaskManager tm = ManagerProvider.getDefault();
         assertNotNull(tm, "TaskManager не создан");
     }
